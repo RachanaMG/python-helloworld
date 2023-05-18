2e1910e7e10c429b4b5a21b41273937be65cf2ee
RachanaMG <90673735+RachanaMG@users.noreply.github.com>
2023-05-18 12:43:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,24 +3,24 @@ import sys
 from io import StringIO
 
 class TestHelloWorld(unittest.TestCase):
-def test_print_output(self):
-expected_output = "Hello world\n"
+  def test_print_output(self):
+    expected_output = "Hello world\n"
 
-# Redirect stdout to a StringIO object
-stdout = sys.stdout
-sys.stdout = StringIO()
+    # Redirect stdout to a StringIO object
+    stdout = sys.stdout
+    sys.stdout = StringIO()
 
-# Run the code that prints "Hello world"
-exec(open("your_script.py").read())
+    # Run the code that prints "Hello world"
+    exec(open("your_script.py").read())
 
-# Get the printed output
-actual_output = sys.stdout.getvalue()
+    # Get the printed output
+    actual_output = sys.stdout.getvalue()
 
-# Restore stdout
-sys.stdout = stdout
+    # Restore stdout
+    sys.stdout = stdout
 
-# Compare the expected and actual outputs
-self.assertEqual(actual_output, expected_output)
+    # Compare the expected and actual outputs
+    self.assertEqual(actual_output, expected_output)
 
 if __name__ == '__main__':
 unittest.main()
