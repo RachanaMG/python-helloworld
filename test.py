f9eebbb4399383af839a87473603dac3a3465f1f
RachanaMG <90673735+RachanaMG@users.noreply.github.com>
2023-05-18 17:15:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,12 @@
 import unittest
-import sys
-from io import StringIO
 
-class TestHelloWorld(unittest.TestCase):
-  def test_print_output(self):
-    expected_output = "Hello, world\n"
-
-    # Redirect stdout to a StringIO object
-    stdout = sys.stdout
-    sys.stdout = StringIO()
-
-    # Run the code that prints "Hello world"
-    exec(open("helloworld.py").read())
-
-    # Get the printed output
-    actual_output = sys.stdout.getvalue()
-
-    # Restore stdout
-    sys.stdout = stdout
-
-    # Compare the expected and actual outputs
-    self.assertEqual(actual_output, expected_output)
-    
+class TestSum(unittest.TestCase):
+    def test_sum(self):
+        num1 = 5
+        num2 = 10
+        expected_sum = num1 + num2
+        actual_sum = num1 + num2
+        self.assertEqual(expected_sum, actual_sum)
+        
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
