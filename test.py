9f9e4515f322bc2e9eb7d96a17f198498ef19090
RachanaMG <90673735+RachanaMG@users.noreply.github.com>
2023-05-23 12:24:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,25 @@
 import unittest
-from helloworld import Greeter
+import sys
+from io import StringIO
 
-class MyTestCase(unittest.TestCase):
-    def test_default_greeting_set(self):
-        greeter = Greeter()
-        self.assertEqual(greeter.message, 'Hello world!')
+class TestHelloWorld(unittest.TestCase):
+    def test_print_output(self):
+        expected_output = "Hello world\n"
 
+        # Redirect stdout to a StringIO object
+        stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run the code that prints "Hello world"
+        exec(open("your_script.py").read())
+
+        # Get the printed output
+        actual_output = sys.stdout.getvalue()
+
+        # Restore stdout
+        sys.stdout = stdout
+
+        # Compare the expected and actual outputs
+        self.assertEqual(actual_output, expected_output)
 if __name__ == '__main__':
     unittest.main()
