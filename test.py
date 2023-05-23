96d5870345e51f3fc876118a0b48238b553f1288
RachanaMG <90673735+RachanaMG@users.noreply.github.com>
2023-05-23 14:36:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import unittest
-import helloworld
 
 class MyTestCase(unittest.TestCase):
     def test_default_greeting_set(self):
-        greeter = Greeter()
-        self.assertEqual(greeter.message, 'Hello world!')
+        self.assertEqual(self.message, 'Hello world!')
 
 if __name__ == '__main__':
     unittest.main()
